ecba8b28b42b31670669e164efc40409ec4afc73
Daniel Ellis <up940148@myport.ac.uk>
2022-01-06 18:30:04 +0000

    Tweaks

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -4,9 +4,33 @@ const ds = new Datastore({ namespace: 'paas-signoff'});
 
 const kind = 'register';
 
+function key(id) {
+  return ds.key([kind, id]);
+}
+
+async function setEntity(reg, val) {
+  const entity = {
+    key: key(reg),
+    data: { name: reg, val },
+  }
+  await ds.save(entity);
+};
+
 module.exports.get = async (reg) => {
   // Get the register
-  const [data] = await ds.get(key(id));
+  let [data] = await ds.get(key(reg));
   console.log(data);
-  return true;
+  if (data === undefined) {
+    // If register doesn't exist:
+    // Create register
+    try {
+      setEntity(reg, 0);
+    } catch (e) {
+      return e;
+    }
+    // Return value
+    [data] = await ds.get(key(reg));
+    console.log(data);
+  }
+  return data;
 }
